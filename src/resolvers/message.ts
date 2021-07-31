0bbd2e339ef98e42cb4e555c9ab5a846f9ed469b
daniel <daniel21dev@gmail.com>
2021-07-30 20:52:40 -0500

    error hanldling whitout trycatch

--- src/resolvers/message.ts
+++ src/resolvers/message.ts
@@ -4,20 +4,16 @@ import { getRandomInt } from '../helpers/getRandomInt';
 export const messageResolvers ={
     Query:{
         getRandomMessage: async()=>{
-            try {
-                // get number of registers
-                const numberOfMessages = await prisma.messagesConnection()
+            // get number of registers
+            const numberOfMessages = await prisma.messagesConnection()
                     .aggregate()
                     .count()
-                // slect a random message
-                const message = await prisma.messages({
-                    first: 1,
-                    skip: getRandomInt(0, numberOfMessages)
-                })
-                return message[0]
-            } catch (error) {
-                console.log( error );
-            }
+            // slect a random message
+            const message = await prisma.messages({
+                first: 1,
+                skip: getRandomInt(0, numberOfMessages)
+            })
+            return message[0]
         },
         getUserMessages: async(_,__, ctx ) =>{
             // check if user is auhtenticated
@@ -39,19 +35,14 @@ export const messageResolvers ={
             if( !ctx.user ){
                 throw new Error('You must be authenticated')
             }
-
-            try {
-                // register message
-                const message = await prisma.createMessage({
-                    text,
-                    user:{
-                        connect: { id: ctx.user.id }
-                    }
-                })
-                return message
-            } catch (error) {
-                console.log( error);
-            }
+            // register message
+            const message = await prisma.createMessage({
+                text,
+                user:{
+                    connect: { id: ctx.user.id }
+                }
+            })
+            return message
         },
         updateMessage: async(_,{ input }, ctx) =>{
             const {text, id} = input
@@ -60,16 +51,12 @@ export const messageResolvers ={
                 throw new Error('You must be authenticated')
             }
 
-            try {
-                // register message updateMessage
-                const message = await prisma.updateMessage({
-                    data:{ text },
-                    where:{ id }
-                })
-                return message
-            } catch (error) {
-                console.log( error);
-            }
+            // register message updateMessage
+            const message = await prisma.updateMessage({
+                data:{ text },
+                where:{ id }
+            })
+            return message
         }
     }
 }
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,50 +5,43 @@ import { createJWT } from '../helpers/jwt';
 export const userResolvers ={
     Query:{
         getUser: async(_,{ id })=>{
-            try {
-                const user = await prisma.user({ id })
-                return user
-            } catch (error) {
-                console.log( error );
+            const user = await prisma.user({ id })
+
+            if( !user ){
+                throw new Error('The user does not exists')
             }
+
+            return user
         }
     },
     Mutation:{
         createUser: async(_,{ input }) =>{
             const { password, email } = input 
-            try {
-                // check if the email is already registered
-                const userExists = await prisma.user({ email })
-                if( userExists ){
-                    throw new Error(`The email ${ input.email } is already registered`);
-                }
-                // Hash password
-                const salt = await bcrypt.genSalt(10);
-                input.password = await bcrypt.hash( password,salt );
-                // register user
-                const user = await prisma.createUser( input );
-                return user
-            } catch (error) {
-                console.log( error);
+            // check if the email is already registered
+            const userExists = await prisma.user({ email })
+            if( userExists ){
+                throw new Error(`The email ${ input.email } is already registered`);
             }
+            // Hash password
+            const salt = await bcrypt.genSalt(10);
+            input.password = await bcrypt.hash( password,salt );
+            // register user
+            const user = await prisma.createUser( input );
+            return user
         },
         loginUser: async(_,{ input })=>{
             const { password, email } = input 
-            try {
-                const user = await prisma.user({ email })
-                if( !user ){    
-                    throw new Error(`User does not exists!`);
-                }
-
-                if( !bcrypt.compareSync( password, user.password ) ){
-                    throw new Error(`Password incorrect`);
-                }
-
-                const token = await createJWT( user.id )
-                return token 
-            } catch (error) {
-                console.log( error );
+            const user = await prisma.user({ email })
+            if( !user ){    
+                throw new Error(`User does not exists!`);
             }
+
+            if( !bcrypt.compareSync( password, user.password ) ){
+                throw new Error(`Password incorrect`);
+            }
+
+            const token = await createJWT( user.id )
+            return token 
         }
     }
 }
